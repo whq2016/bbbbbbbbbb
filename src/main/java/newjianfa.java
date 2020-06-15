851814fb9425567be17a8681202c877543c46e29
whq2016 <191562758@qq.com>
2020-06-16 03:36:24 +0800

    bddd修改

--- src/main/java/newjianfa.java
+++ src/main/java/newjianfa.java
@@ -1,5 +1,9 @@
 public class newjianfa {
     public static void main(String[] args) {
         System.out.println("新剑法");
+        System.out.println("新剑法");
+        System.out.println("新剑法");
+        System.out.println("新剑法");
+        System.out.println("新剑法");
     }
 }
