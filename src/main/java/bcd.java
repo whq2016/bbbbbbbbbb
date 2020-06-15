17eccc4f2e893fdcbd22255200d628b7b4b30bfc
whq2016 <191562758@qq.com>
2020-06-15 17:17:17 +0800

    abc提交

--- src/main/java/bcd.java
+++ src/main/java/bcd.java
@@ -1,5 +1,8 @@
 public class bcd {
     public static void main(String[] args) {
         System.out.println("测试");
+        System.out.println("fadf");
+        System.out.println("fasdfasdfasdf");
+
     }
 }
